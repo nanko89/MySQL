b118487d2cbba7ac7647c2e5932de0fb7dfd5386
nanko89 <natali.v.angelova@gmail.com>
2022-01-12 18:28:59 +0200

    MySQL Database Programmability and Transactions LAB

--- Database Programmability and Transactions/Database Programmability and Transactions LAB.sql
+++ Database Programmability and Transactions/Database Programmability and Transactions LAB.sql
@@ -67,3 +67,4 @@ BEGIN
 	INSERT INTO `deleted_employees`(`first_name`,`last_name`, `middle_name`,`job_title`,`department_id`,`salary`)
     VALUES (OLD.`first_name`,OLD.`last_name`, OLD.`middle_name`,OLD.`job_title`,OLD.`department_id`,OLD.`salary`);
 END &&
+DELIMITER ;
