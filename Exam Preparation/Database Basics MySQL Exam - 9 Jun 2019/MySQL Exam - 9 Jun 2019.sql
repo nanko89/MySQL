20f42b488e54f1738a8bab4563d96f85b196efb8
nanko89 <natali.v.angelova@gmail.com>
2022-01-18 17:21:05 +0200

    MySQL Exam - 9 Jun 2019

--- Exam Preparation/Database Basics MySQL Exam - 9 Jun 2019/MySQL Exam - 9 Jun 2019.sql
+++ Exam Preparation/Database Basics MySQL Exam - 9 Jun 2019/MySQL Exam - 9 Jun 2019.sql
@@ -122,3 +122,60 @@ LEFT JOIN clients AS cl
 ON ba.client_id = cl.id
 ORDER BY c.id DESC;
 
+ # ---------- Ex 8 Top 5 Employees ------------
+
+SELECT CONCAT (e.first_name, ' ' , e.last_name) AS `name`, e.started_on, COUNT(ec.client_id) AS count_of_clients
+FROM employees AS e
+LEFT JOIN employees_clients AS ec
+ON e.id = ec.employee_id
+GROUP BY e.id 
+ORDER BY count_of_clients DESC, e.id
+LIMIT 5;
+
+ # ---------- Ex 9 Branch cards ------------
+
+SELECT b.`name`, COUNT(c.id) AS count_of_cards
+FROM branches AS b
+LEFT JOIN employees AS e
+ON b.id = e.branch_id
+LEFT JOIN employees_clients AS ec
+ON e.id = ec.employee_id
+LEFT JOIN clients AS cl
+ON ec.client_id = cl.id
+LEFT JOIN bank_accounts AS ba
+ON ba.client_id = cl.id
+LEFT JOIN cards AS c
+ON ba.id = c.bank_account_id
+GROUP BY b.id
+ORDER BY count_of_cards DESC, b.`name`; 
+
+ # ---------- Ex 10 Extract client cards count ------------
+DELIMITER &&
+CREATE FUNCTION udf_client_cards_count(`name` VARCHAR(30))  
+RETURNS INT
+DETERMINISTIC
+BEGIN 
+	RETURN (
+	SELECT COUNT(c.id) 
+	FROM clients AS cl
+	JOIN bank_accounts AS ba
+	ON ba.client_id = cl.id
+	JOIN cards AS c
+	ON c.bank_account_id = ba.id
+	WHERE cl.full_name = `name`);
+    END &&
+
+DELIMITER ;
+
+ # ---------- Ex 11 Extract Client Info ------------
+ DELIMITER &&
+ 
+CREATE PROCEDURE udp_clientinfo (client_name VARCHAR (50))
+BEGIN 
+	SELECT c.full_name, c.age, ba.account_number, CONCAT ('$', ba.balance)
+	FROM clients AS c
+	JOIN bank_accounts AS ba
+	ON ba.client_id = c.id
+	WHERE c.full_name = client_name;
+END &&
+DELIMITER ;
