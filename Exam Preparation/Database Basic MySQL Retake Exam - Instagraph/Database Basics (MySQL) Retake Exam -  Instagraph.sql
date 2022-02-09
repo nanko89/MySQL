fd8a8f5faf2468b3a70fb04ca93be7536ff3fd77
nanko89 <natali.v.angelova@gmail.com>
2022-02-09 18:25:38 +0200

    Exam Preparation/Database Basic MySQL Retake Exam - Instagraph

---
+++ Exam Preparation/Database Basic MySQL Retake Exam - Instagraph/Database Basics (MySQL) Retake Exam -  Instagraph.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE instagraph_db;
+USE instagraph_db;
+
+---------------- 1 -----------------
+
+CREATE TABLE pictures (
+id INT PRIMARY KEY AUTO_INCREMENT,
+`path` VARCHAR (255) NOT NULL,
+size DECIMAL (10,2) NOT NULL 
+);
+
+CREATE TABLE users(
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR (30) NOT NULL UNIQUE,
+`password` VARCHAR (30) NOT NULL,
+profile_picture_id INT, 
+CONSTRAINT fk_user_picture
+FOREIGN KEY (profile_picture_id)
+REFERENCES pictures (id)
+);
+
+CREATE TABLE posts (
+id INT PRIMARY KEY AUTO_INCREMENT,
+caption VARCHAR (255) NOT NULL,
+user_id INT NOT NULL,
+picture_id INT NOT NULL,
+CONSTRAINT fk_post_user
+FOREIGN KEY (user_id)
+REFERENCES users (id),
+CONSTRAINT fk_post_picture
+FOREIGN KEY (picture_id)
+REFERENCES pictures (id) 
+);
+
+CREATE TABLE comments (
+id INT PRIMARY KEY AUTO_INCREMENT,
+content VARCHAR (255) NOT NULL,
+user_id INT NOT NULL,
+post_id INT NOT NULL,
+CONSTRAINT fk_comment_user
+FOREIGN KEY (user_id)
+REFERENCES users (id),
+CONSTRAINT fk_comment_post
+FOREIGN KEY (post_id)
+REFERENCES posts(id)
+);
+
+CREATE TABLE users_followers (
+user_id	INT,
+follower_id	INT,
+KEY key_user_follower (user_id, follower_id),
+CONSTRAINT fk_users_followers_users
+FOREIGN KEY (user_id)
+REFERENCES users (id),
+CONSTRAINT fk_users_followers_follower
+FOREIGN KEY (follower_id)
+REFERENCES users(id)
+);
+
+---------------- 2 -----------------
+
+INSERT INTO comments (content, user_id, post_id)
+SELECT CONCAT('Omg!',u.`username`,'!This is so cool!'), CEIL(p.id * 3 / 2), p.id FROM posts AS p
+JOIN users AS u
+ON u.id  = p.user_id
+WHERE p.id BETWEEN 1 AND 10;
+
+
+---------------- 3 -----------------
+
+UPDATE users
+JOIN (
+SELECT COUNT(uf.follower_id) AS follower
+FROM users AS u
+JOIN  users_followers AS uf
+ON u.id = uf.user_id
+GROUP BY u.id
+) AS result
+SET profile_picture_id = (IF (result.follower != 0, result.follower, id))
+WHERE profile_picture_id IS NULL;
+
+---------------- 4 -----------------
+
+DELETE 
+ FROM users 
+ WHERE id NOT IN (SELECT user_id FROM users_followers) 
+ AND id NOT IN (SELECT follower_id FROM users_followers);
+ 
+---------------- 5 -----------------
