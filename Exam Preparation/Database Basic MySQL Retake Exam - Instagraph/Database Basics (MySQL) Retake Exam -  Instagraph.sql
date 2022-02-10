bded1673c2b9ab60472fb51b19ae17ea7ae14ad6
nanko89 <natali.v.angelova@gmail.com>
2022-02-10 13:41:22 +0200

    Exam Preparation/Database Basic MySQL Retake Exam - Instagraph

--- Exam Preparation/Database Basic MySQL Retake Exam - Instagraph/Database Basics (MySQL) Retake Exam -  Instagraph.sql
+++ Exam Preparation/Database Basic MySQL Retake Exam - Instagraph/Database Basics (MySQL) Retake Exam -  Instagraph.sql
@@ -60,30 +60,102 @@ REFERENCES users(id)
 ---------------- 2 -----------------
 
 INSERT INTO comments (content, user_id, post_id)
-SELECT CONCAT('Omg!',u.`username`,'!This is so cool!'), CEIL(p.id * 3 / 2), p.id FROM posts AS p
-JOIN users AS u
-ON u.id  = p.user_id
+SELECT CONCAT('Omg!',u.`username`,'!This is so cool!'), 
+		CEIL(p.id * 3 / 2), p.id 
+	FROM posts AS p
+			JOIN 
+	users AS u ON u.id  = p.user_id
 WHERE p.id BETWEEN 1 AND 10;
 
 
 ---------------- 3 -----------------
 
 UPDATE users
-JOIN (
-SELECT COUNT(uf.follower_id) AS follower
-FROM users AS u
-JOIN  users_followers AS uf
-ON u.id = uf.user_id
-GROUP BY u.id
-) AS result
-SET profile_picture_id = (IF (result.follower != 0, result.follower, id))
-WHERE profile_picture_id IS NULL;
+        JOIN
+    (SELECT 
+        COUNT(uf.follower_id) AS follower
+    FROM
+        users AS u
+    JOIN users_followers AS uf ON u.id = uf.user_id
+    GROUP BY u.id) AS result 
+SET 
+    profile_picture_id = (IF(result.follower != 0,
+        result.follower,
+        id))
+WHERE
+    profile_picture_id IS NULL;
 
 ---------------- 4 -----------------
 
-DELETE 
- FROM users 
- WHERE id NOT IN (SELECT user_id FROM users_followers) 
- AND id NOT IN (SELECT follower_id FROM users_followers);
+DELETE FROM users 
+WHERE
+    id NOT IN (SELECT 
+        user_id
+    FROM
+        users_followers)
+    AND id NOT IN (SELECT 
+        follower_id
+    FROM
+        users_followers);
  
 ---------------- 5 -----------------
+
+SELECT 
+    id, username
+FROM
+    users
+ORDER BY id;
+
+---------------- 6 -----------------
+
+SELECT 
+    u.id, u.username
+FROM
+    users AS u
+        JOIN
+    users_followers AS uf ON u.id = uf.user_id
+WHERE
+    uf.user_id = uf.follower_id
+ORDER BY u.id;
+
+---------------- 7 -----------------
+
+SELECT 
+    *
+FROM
+    pictures
+WHERE
+    size > 50000
+        AND (`path` LIKE ('%jpeg%')
+        OR `path` LIKE ('%png%'))
+ORDER BY size DESC;
+
+---------------- 8 -----------------
+
+SELECT 
+    c.id, CONCAT(u.username, ' : ', c.content) AS full_name
+FROM
+    users AS u
+        JOIN
+    comments AS c ON c.user_id = u.id
+ORDER BY c.id DESC;
+
+---------------- 9 -----------------
+
+SELECT 
+    u.id, u.username, CONCAT(p.size, 'KB') AS size
+FROM
+    users AS u
+        JOIN
+    pictures AS p ON p.id = u.profile_picture_id
+WHERE
+    (SELECT 
+            COUNT(u2.profile_picture_id)
+        FROM
+            users AS u2
+        WHERE
+            u.profile_picture_id = u2.profile_picture_id) > 1
+ORDER BY u.id;
+	
+
+
