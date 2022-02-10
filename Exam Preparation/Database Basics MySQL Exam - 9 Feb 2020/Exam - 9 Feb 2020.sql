46e0b0637a54d5d112c2daf2b9ff3169360ac988
nanko89 <natali.v.angelova@gmail.com>
2022-02-10 16:58:10 +0200

    Exam - 9 Feb 2020

--- Exam Preparation/Database Basics MySQL Exam - 9 Feb 2020/Exam - 9 Feb 2020.sql
+++ Exam Preparation/Database Basics MySQL Exam - 9 Feb 2020/Exam - 9 Feb 2020.sql
@@ -162,14 +162,69 @@ ORDER BY
         
 -- 8----------------------------------------
 
-	SELECT MAX(sd.speed) AS max_speed, t.`name` AS town_name
-    FROM 
+	SELECT 
+    MAX(sd.speed) AS max_speed, t.`name` AS town_name
+FROM
+    skills_data AS sd
+        RIGHT JOIN
+    players AS p ON p.skills_data_id = sd.id
+        RIGHT JOIN
+    teams AS te ON te.id = p.team_id
+        RIGHT JOIN
+    stadiums AS s ON s.id = te.stadium_id
+        RIGHT JOIN
+    towns AS t ON t.id = s.town_id
+WHERE
+    te.name != 'Devify'
+GROUP BY t.name
+ORDER BY max_speed DESC , t.name;
 
 -- 9----------------------------------------
--- 10---------------------------------------
--- 11---------------------------------------
--- 12---------------------------------------
--- 13---------------------------------------
--- 14---------------------------------------
--- 15---------------------------------------
 
+SELECT c.`name`, COUNT(p.id) AS total_count_of_players, SUM(p.salary) AS total_sum_of_salaries
+FROM players AS p 
+        RIGHT JOIN
+    teams AS te ON te.id = p.team_id
+        RIGHT JOIN
+    stadiums AS s ON s.id = te.stadium_id
+        RIGHT JOIN
+    towns AS t ON t.id = s.town_id
+		RIGHT JOIN
+	countries AS c ON c.id = t.country_id
+GROUP BY c.`name`
+ORDER BY total_count_of_players DESC, c.`name`;
+
+
+-- 10---------------------------------------
+
+
+DELIMITER %%
+CREATE FUNCTION udf_stadium_players_count (stadium_name VARCHAR(30)) 
+RETURNS INTEGER
+	DETERMINISTIC
+BEGIN 
+	RETURN (SELECT COUNT(p.id) 
+	FROM players AS p 
+        RIGHT JOIN
+    teams AS te ON te.id = p.team_id
+        RIGHT JOIN
+    stadiums AS s ON s.id = te.stadium_id
+WHERE s.`name` = stadium_name);
+END %%
+
+
+-- 11---------------------------------------
+
+CREATE PROCEDURE udp_find_playmaker (min_dribble_points INT , t_name VARCHAR (45))
+	BEGIN
+		SELECT CONCAT(p.first_name, ' ' , p.last_name) AS full_name, p.age, p.salary, sd.dribbling, sd.speed, t.`name` AS team_name 
+		FROM
+			skills_data AS sd
+				RIGHT JOIN
+			players AS p ON p.skills_data_id = sd.id
+				RIGHT JOIN
+			teams AS t ON t.id = p.team_id
+		WHERE t.`name` = t_name AND sd.dribbling > min_dribble_points
+		ORDER BY sd.speed DESC
+		LIMIT 1;
+    END %%
