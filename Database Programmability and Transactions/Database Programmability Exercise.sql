3cdcd1678f5319eec855623636c902161f1ce42d
nanko89 <natali.v.angelova@gmail.com>
2022-02-11 14:26:39 +0200

    Update Exercise 16

--- Database Programmability and Transactions/Database Programmability Exercise.sql
+++ Database Programmability and Transactions/Database Programmability Exercise.sql
@@ -208,17 +208,33 @@ FOR EACH ROW
 # ------------- Ex 16 ---------------
 DELIMITER ;
 
-CREATE TABLE `notification_emails`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`recipient` INT,
+CREATE TABLE `logs` (
+`log_id` INT PRIMARY KEY AUTO_INCREMENT,
+`account_id` INT,
+`old_sum` DECIMAL (19,4),
+`new_sum` DECIMAL (19,4)
+);
+
+DELIMITER %%
+CREATE TRIGGER tr_on_uppdate 
+AFTER UPDATE
+ON `accounts`
+FOR EACH ROW
+	BEGIN
+		INSERT INTO `logs` (`account_id`, `old_sum`, `new_sum`)
+        VALUES (OLD.`id`, OLD.`balance`, NEW.`balance`);
+    END %%
+
+CREATE TABLE notification_emails(
+id INT PRIMARY KEY AUTO_INCREMENT,
+recipient INT NOT NULL,
 `subject` VARCHAR (150),
-`body` VARCHAR (150)
- ); 
+body TEXT
+ ) %% 
  
- DELIMITER &&
  
- CREATE TRIGGER `tr_notification`
- AFTER UPDATE
+ CREATE TRIGGER tr_notification
+ BEFORE INSERT
  ON `logs`
  FOR EACH ROW
 	BEGIN
@@ -227,6 +243,6 @@ CREATE TABLE `notification_emails`(
         CONCAT('Balance change for account: ', NEW.`account_id`),
         CONCAT('On', DATE_FORMAT(NOW(), '%b %d %Y %r'),' your balance was changed from ',
         ROUND(NEW.old_sum, 2), ' to ', ROUND(NEW.new_sum, 2), '.'));
-    END &&
+    END %%
 
  
