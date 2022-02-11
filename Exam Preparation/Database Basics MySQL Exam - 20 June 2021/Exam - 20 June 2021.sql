cbe1b2d033b566c1bc77dff55d23810e7edb3217
nanko89 <natali.v.angelova@gmail.com>
2022-02-12 00:16:08 +0200

    Update Exam - 20 June 2021

--- Exam Preparation/Database Basics MySQL Exam - 20 June 2021/Exam - 20 June 2021.sql
+++ Exam Preparation/Database Basics MySQL Exam - 20 June 2021/Exam - 20 June 2021.sql
@@ -113,9 +113,106 @@ ORDER BY id;
 
 -- 6----------------------------
 
+SELECT 
+    d.first_name, d.last_name, c.make, c.model, c.mileage
+FROM
+    drivers AS d
+        JOIN
+    cars_drivers AS cd ON d.id = cd.driver_id
+        JOIN
+    cars AS c ON c.id = cd.car_id
+    WHERE c.mileage Is NOT NULL
+ORDER BY c.mileage DESC , d.first_name;
 
 -- 7----------------------------
+
+SELECT 
+    c.id,
+    c.make,
+    c.mileage,
+    COUNT(co.id) AS count_of_courses,
+    ROUND(AVG(co.bill), 2) AS avg_bill
+FROM
+    cars AS c
+        LEFT JOIN
+    courses AS co ON c.id = co.car_id
+GROUP BY c.id
+HAVING count_of_courses != 2
+ORDER BY count_of_courses DESC , c.id;
+
 -- 8----------------------------
+
+SELECT 
+    cl.full_name, COUNT(c.id) AS count_car, SUM(co.bill)
+FROM
+    clients AS cl
+        JOIN
+    courses AS co ON cl.id = co.client_id
+        JOIN
+    cars AS c ON c.id = co.car_id
+WHERE
+    cl.full_name LIKE '_a%'
+GROUP BY cl.full_name
+HAVING count_car > 1
+ORDER BY cl.full_name;
+
 -- 9----------------------------
--- 1o----------------------------
+
+SELECT 
+    a.`name`,
+    (IF(HOUR(co.`start`) BETWEEN 6 AND 20,
+        'Day',
+        'Night')) AS day_time,
+    co.bill,
+    cl.full_name,
+    c.make,
+    c.model,
+    ca.`name`
+FROM
+    clients AS cl
+        JOIN
+    courses AS co ON cl.id = co.client_id
+        JOIN
+    addresses AS a ON a.id = co.from_address_id
+        JOIN
+    cars AS c ON c.id = co.car_id
+        JOIN
+    categories AS ca ON ca.id = c.category_id
+ORDER BY co.id;
+
+-- 10----------------------------
+
+DELIMITER %%
+
+CREATE FUNCTION udf_courses_by_client (phone_num VARCHAR (20)) 
+RETURNS INTEGER
+DETERMINISTIC 
+	BEGIN
+    RETURN (SELECT COUNT(client_id) FROM courses
+			WHERE client_id = (SELECT id FROM clients
+			WHERE phone_number = phone_num) );
+    END %%
+    
 -- 11----------------------------
+
+CREATE PROCEDURE udp_courses_by_address (address_name VARCHAR (100))
+		BEGIN
+				SELECT a.`name`, cl.full_name, 
+			(CASE
+				WHEN co.bill <= 20 THEN 'Low'
+				WHEN co.bill <= 30 THEN 'Medium'
+				ELSE 'High'
+			END) AS level_of_bill,
+			c.make, c.`condition`, ca.`name` AS cat_name
+				FROM  clients AS cl
+					JOIN
+				courses AS co ON cl.id = co.client_id
+					JOIN
+				addresses AS a ON a.id = co.from_address_id
+					JOIN
+				cars AS c ON c.id = co.car_id
+					JOIN
+				categories AS ca ON ca.id = c.category_id
+				WHERE a.`name` = address_name
+				ORDER BY c.make, cl.full_name;
+        END %%
